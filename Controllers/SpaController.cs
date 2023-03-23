30a6887ca1b95442b5e2648d28e5c9782f7a76ae
Donatas Stankevicius <U1953937@unimail.hud.ac.uk>
2023-03-23 15:41:42 +0000

    Add project files.

---
+++ Controllers/SpaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NhsImsApp.Controllers
+{
+    public class SpaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return File("~/dist/index.html", "text/html");
+        }
+    }
+}
