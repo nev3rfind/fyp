50f75b6f0adf0808080cd998c3ecd8ec9cdf9521
Donatas Stankevičius <57880277+nev3rfind@users.noreply.github.com>
2023-03-23 13:59:42 +0000

    added default routeCreate README.md

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace Nhs_Ims_App
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "SPA",
+                url: "{*catchall}",
+                defaults: new { controller = "Spa", action = "Index" }
             );
         }
     }
